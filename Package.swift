0cd208ba0fca29d7f3b756ebc9ca5dadbeeeee89
Johannes Schriewer <hallo@dunkelstern.de>
2015-12-30 08:24:55 +0100

    Add Swift package description

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  UnchainedSocket
+//
+//  Created by Johannes Schriewer on 2015-12-30.
+//  Copyright © 2015 Johannes Schriewer. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "UnchainedSocket"
+)
